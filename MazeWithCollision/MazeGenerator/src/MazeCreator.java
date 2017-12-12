8f570320c3ecae5307f5d4749c75272612db1008
mholks <33465951+mholks@users.noreply.github.com>
2017-12-12 16:35:18 +0100

    Update MazeCreator.java

--- MazeWithCollision/MazeGenerator/src/MazeCreator.java
+++ MazeWithCollision/MazeGenerator/src/MazeCreator.java
@@ -1,4 +1,13 @@
+/**
+ * @author AmazingGroup 
+ * This class represents the Strategy interface. Which
+ * makes the maze class(context) independent on how the maze algorithms
+ * are implemented.
+ */
+
 public interface MazeCreator {
-	public void setUpStructure(Cell[][] mazeFields);	
-	public void createMaze(Cell startingField,Cell endField);
+	public void setUpStructure(Cell[][] mazeFields);
+
+	public void createMaze(Cell startingField, Cell endField);
 }
+
